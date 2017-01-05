9aa93c7356ec68c5ce9b3c7ad48b8c35a8fe7680
Derek Joshua Lin <DerekJLin@Dereks-MacBook-Pro.local>
2017-01-04 21:48:45 -0800

    finished assignment: added css transitions and looped revealingpoints function (TRIED)

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,18 @@
 
 var animatePoints = function(){
   var points = document.getElementsByClassName('point');
-
-  var revealFirstPoint = function(){
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)"; points[0].style.msTransform = "scaleX(1) translateY(0)"; points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+  
+  function revealPoint(i){
+    points[i].style.opacity = 1;
+    points[i].style.transform = "scaleX(1) translateY(0)"; points[i].style.msTransform = "scaleX(1) translateY(0)"; points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
-
-  var revealSecondPoint = function(){
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)"; points[1].style.msTransform = "scaleX(1) translateY(0)"; points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  var revealThirdPoint = function(){
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)"; points[2].style.msTransform = "scaleX(1) translateY(0)"; points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
+  
+  for (var i=0;i<3; i++){
+    revealPoint(i);
+  }
 
 };
 
+/*
 animatePoints();
+*/
