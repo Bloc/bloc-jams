40419652ef6269c7e656e09de0c24c46d1d0f307
Derek Joshua Lin <DerekJLin@Dereks-MacBook-Pro.local>
2017-01-04 23:52:14 -0800

    final changes made for assignment: found out that the page wasn't animating probably becuase the script was being called too early, wiat but i put it at the end of all body????why????

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,17 +2,27 @@
 var animatePoints = function(){
   var points = document.getElementsByClassName('point');
   
-  function revealPoint(i){
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)"; points[i].style.msTransform = "scaleX(1) translateY(0)"; points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+  function revealPoint(index){
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)"; 
+    points[index].style.msTransform = "scaleX(1) translateY(0)"; 
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
   
-  for (var i=0;i<3; i++){
+  for (var i=0;i<points.length; i++){
     revealPoint(i);
   }
 
 };
 
-/*
-animatePoints();
-*/
+
+
+
+window.onload = function() {
+  window.addEventListener('scroll', function(event){
+    console.log(event);
+    animatePoints();
+  });
+ }
+
+
