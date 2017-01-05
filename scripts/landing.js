133273fbc0d96f3834a361b2032c17d6555a86ae
Derek Joshua Lin <DerekJLin@Dereks-MacBook-Pro.local>
2017-01-05 00:54:06 -0800

    done with checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
+  var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(){
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points){
   
   function revealPoint(index){
     points[index].style.opacity = 1;
@@ -19,9 +19,15 @@ var animatePoints = function(){
 
 
 window.onload = function() {
+  if (window.innerHeight > 950){
+   animatePoints(pointsArray);
+ }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+ 
   window.addEventListener('scroll', function(event){
-    console.log(event);
-    animatePoints();
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance)
+      animatePoints(pointsArray);
   });
  }
 
