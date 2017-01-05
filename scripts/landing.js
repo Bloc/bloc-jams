d346331dc573ab2f656e8b9d69773819e7e20bd9
Derek Joshua Lin <DerekJLin@Dereks-MacBook-Pro.local>
2017-01-05 09:33:00 -0800

    addd scrolling animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -19,12 +19,12 @@ var animatePoints = function(points){
 
 
 window.onload = function() {
-  if (window.innerHeight > 950){
-   animatePoints(pointsArray);
- }
+  
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
- 
+ if (window.innerHeight > 950){
+   animatePoints();
+ }
   window.addEventListener('scroll', function(event){
     if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance)
       animatePoints(pointsArray);
