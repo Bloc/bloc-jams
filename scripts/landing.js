cc144dda1e019cde5a5fc773aeded79abb7dd914
Derek Joshua Lin <DerekJLin@Dereks-MacBook-Pro.local>
2017-01-05 10:12:01 -0800

    Trial1 :forEach loop for revealing points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
   var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points){
+/*var animatePoints = function(points){
   
   function revealPoint(index){
     points[index].style.opacity = 1;
@@ -9,23 +9,36 @@ var animatePoints = function(points){
     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   }
   
-  for (var i=0;i<points.length; i++){
-    revealPoint(i);
-  }
+  forEach(points,revealPoint);
 
-};
+};*/
 
+/*WHY DOESN'T THE ABOVE WORK? COULD NOT ANIMATE HMM....*/
+
+var animatePoints = function(points){
+  forEach(points,revealPoint);
+}
+
+function revealPoint(index){
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)"; 
+    points[index].style.msTransform = "scaleX(1) translateY(0)"; 
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+}
 
 
 
 window.onload = function() {
   
+   if (window.innerHeight > 950){
+   animatePoints(pointsArray);
+ }  
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
- if (window.innerHeight > 950){
-   animatePoints();
- }
+ 
   window.addEventListener('scroll', function(event){
+    
+    /*when would this ever be null?*/
     if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance)
       animatePoints(pointsArray);
   });
---
+++ scripts/utilities.js
@@ -0,0 +1,10 @@
+
+
+/*foreach loop*/
+
+function forEach(array, callback){
+  for(var i=0;i<array.length;i++)
+    callback(array[i]);
+}
+
+
